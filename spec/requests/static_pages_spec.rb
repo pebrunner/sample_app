b4c468930bd91a8301a63460a3319f35a46ccc1f
= <=>
2014-07-25 16:35:04 -0400

    Fixed full_title functionality - take 2

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -54,7 +54,7 @@ describe "Static pages" do
 
     it "should have the title 'Contact'" do
       visit '/static_pages/contact'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact Us")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
     end
   end
 
